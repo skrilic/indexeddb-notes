c20ebe85e245d641958e7cc207c57e0b1ba46c9a
Slaven Krilic <skrilic@gmail.com>
2022-12-19 15:05:18 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
 import AppRoutes from './components/AppRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h3>Notes</h3>
-      </header>
+    <div className="text-inherit flex justify-center">
       <AppRoutes />
     </div>
   );
