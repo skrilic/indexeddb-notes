e784833e7bbbab9ef1572acada9fcb13d7d3a79e
Slaven Krilic <skrilic@gmail.com>
2023-02-01 12:27:56 +0100

    ...

---
+++ src/components/ReadingPlanList.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const ReadingPlanList = ({ files }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleSelectFile = file => {
+    setSelectedFile(file);
+  };
+
+  return (
+    <div>
+      <select onChange={e => handleSelectFile(e.target.value)}>
+        {files.map(file => (
+          <option key={file.name} value={file.name}>
+            {file.name}
+          </option>
+        ))}
+      </select>
+      {selectedFile && (
+        <ul>
+          {files
+            .find(file => file.name === selectedFile)
+            .children.map(child => (
+              <li key={child.name}>{child.name}</li>
+            ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ReadingPlanList;
