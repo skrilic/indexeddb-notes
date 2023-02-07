955a601b0bba6edcf94343879edb71c61f277ef1
Slaven Krilic <skrilic@gmail.com>
2023-02-07 17:20:16 +0100

    ...

--- src/components/ReadingPlanList.jsx
+++ src/components/ReadingPlanList.jsx
@@ -1,31 +1,52 @@
 import React, { useState } from 'react';
-import files from "../data/files.json";
+import BRP from "../data/BibleReadingPlan.json";
+import { v4 as uuidv4 } from "uuid";
 
-const ReadingPlanList = ({ files }) => {
-  const [selectedFile, setSelectedFile] = useState(null);
+const ReadingPlanList = () => {
+    const reads = BRP.readingPlan;
+    // const files = Files.files;
+    const [selectedFile, setSelectedFile] = useState(null);
 
   const handleSelectFile = file => {
     setSelectedFile(file);
   };
 
+    const groupBy = function(inArray, key) {
+        const initialValue = {};
+
+        return inArray.reduce(
+          function(accumulator, currentValue) {
+              (accumulator[currentValue[key]] = accumulator[currentValue[key]] || []).push(currentValue);
+              return accumulator;
+          }, initialValue);
+    };
+
+
+    const dayReadings = groupBy(reads, "day");
+    const keyCollection = [];
+
+    for(var key in dayReadings) {
+        if(dayReadings.hasOwnProperty(key)) {
+            keyCollection.push(key);
+        }
+    }
+
+    keyCollection.forEach(item => console.log(dayReadings[item]));
+
   return (
     <div>
-      <select onChange={e => handleSelectFile(e.target.value)}>
-        {files.map(file => (
-          <option key={file.name} value={file.name}>
-            {file.name}
-          </option>
-        ))}
-      </select>
-      {selectedFile && (
         <ul>
-          {files
-            .find(file => file.name === selectedFile)
-            .children.map(child => (
-              <li key={child.name}>{child.name}</li>
-            ))}
+            {keyCollection.map(item =>
+                <li key={uuidv4()}>
+                    {item}
+                <ul>
+                    <li key={uuidv4()}>
+                        {dayReadings[item].map(dayRead => <p>{dayRead.book}, {dayRead.chapter}: {dayRead.verse}</p>)}
+                    </li>
+                </ul>
+                </li>
+            )}
         </ul>
-      )}
     </div>
   );
 };
